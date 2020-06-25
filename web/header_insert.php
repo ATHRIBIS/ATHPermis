6bae02fc2eb6729c596430a2a0d70b9aebaf2714
ATH <schaeffer.aymeric@gmail.com>
2020-06-25 11:05:39 +0200

    Add header_insert.php and background gradient

---
+++ web/header_insert.php
@@ -0,0 +1,9 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Entrée</a></li>
+            <li><a href="#">Profil</a></li>
+        </ul>
+    </nav>
+</header>
--- web/index.php
+++ web/index.php
@@ -1,1 +1,12 @@
 <?php
+?>
+<html lang="fr">
+    <head>
+        <title>ATH Permis</title>
+        <meta charset="UTF-8" />
+        <link href="website_background.css" rel="stylesheet">
+    </head>
+    <body>
+    <?php include 'header_insert.php'; ?>
+    </body>
+</html>
